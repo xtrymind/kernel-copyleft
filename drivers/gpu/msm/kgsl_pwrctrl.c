6e29d854439bf045b63845ef4a5866dd772ed2c5
flar2 <asegaert@gmail.com>
2015-09-06 01:56:35 -0400

    kgsl: less aggressive throttling

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -154,9 +154,14 @@ static void _ab_buslevel_update(struct kgsl_pwrctrl *pwr,
 static unsigned int _adjust_pwrlevel(struct kgsl_pwrctrl *pwr, int level,
 					struct kgsl_pwr_constraint *pwrc)
 {
-	unsigned int max_pwrlevel = max_t(unsigned int, pwr->thermal_pwrlevel,
+	unsigned int therm_pwrlevel, max_pwrlevel, min_pwrlevel;
+
+	if (pwr->thermal_pwrlevel > 0)
+		therm_pwrlevel = pwr->thermal_pwrlevel - 1;
+
+	max_pwrlevel = max_t(unsigned int, therm_pwrlevel,
 		pwr->max_pwrlevel);
-	unsigned int min_pwrlevel = max_t(unsigned int, pwr->thermal_pwrlevel,
+	min_pwrlevel = max_t(unsigned int, therm_pwrlevel,
 		pwr->min_pwrlevel);
 
 	switch (pwrc->type) {
