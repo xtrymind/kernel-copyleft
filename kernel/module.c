4e680cc2f6471b67136e3c6893e2cbf72d84f916
Dede Dindin Qudsy <xtrymind@gmail.com>
2017-12-11 13:02:54 +0700

    disable modversion for core_ctl

--- kernel/module.c
+++ kernel/module.c
@@ -1168,10 +1168,13 @@ static int check_version(Elf_Shdr *sechdrs,
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
 
-	/* ignore modversion for texfat */
+	/* ignore magic version */
 	if(!strncmp("texfat", mod->name, 6))
 		return 0;
 
+	if(!strncmp("core_ctl", mod->name, 8))
+		return 1;
+ 
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
 		return 1;
